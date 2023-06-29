c99b54d69de007d8c7d148e2eab8e67abd93ee75
VadimRuslanch <Merzlyy.Pes@yandex.ru>
2023-06-30 02:11:30 +0300

    fix auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/UnauthorizedError');
-
-const { NODE_ENV, JWT_SECRET = 'dev-secret' } = process.env;
+const JWT_SECRET = require('../config');
 
 const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new UnauthorizedError('Необходима авторизация'));
+  }
   const token = req.cookies.jwt;
   let payload;
 
   try {
     payload = jwt.verify(
       token,
-      `${NODE_ENV === 'production'
-        ? JWT_SECRET
-        : 'dev-secret'}`,
+      JWT_SECRET,
     );
   } catch (err) {
     return next(new UnauthorizedError('Неверный электронный адрес или пароль'));
