5586b3af41de6fc7b708c2235b84a3b72b0068c5
VadimRuslanch <Merzlyy.Pes@yandex.ru>
2023-06-29 21:33:25 +0300

    returning the old version

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-
-const { login, createUser } = require('./controllers/users');
 const routes = require('./routes/index');
-
 const auth = require('./middlewares/auth');
-const corsErr = require('./middlewares/corsErr');
+const { login, createUser } = require('./controllers/users');
 const { validationCreateUser, validationLogin } = require('./middlewares/validation');
-
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(requestLogger);
 
 const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use(requestLogger);
 app.post('/signin', validationLogin, login);
 app.post('/signup', validationCreateUser, createUser);
 app.use(auth);
 app.use(routes);
-app.use(corsErr);
 app.use(errorLogger);
 
 async function connect() {
@@ -34,16 +27,20 @@ async function connect() {
     await mongoose.connect('mongodb://localhost:27017/mestodb', {
       family: 4,
     });
+    // eslint-disable-next-line no-console
     console.log(`App connected ${MONGO_URL}`);
     await app.listen(PORT);
+    // eslint-disable-next-line no-console
     console.log(`App listening on port ${PORT}`);
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log(err);
   }
 }
 
 app.use(errors());
 app.use((err, __, res, next) => {
+  // eslint-disable-next-line no-console
   console.log(err);
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
