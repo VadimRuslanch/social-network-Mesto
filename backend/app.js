e4adcf8966e0c1b96f4a15befd13d43b147a999d
VadimRuslanch <Merzlyy.Pes@yandex.ru>
2023-06-30 01:29:51 +0300

    fix backend

--- backend/app.js
+++ backend/app.js
@@ -2,30 +2,32 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const corsErr = require('./middlewares/cors');
 const routes = require('./routes');
-const config = require('./config');
+const { MONGODB_URI, PORT } = require('./config');
 
 const app = express();
-app.use(cors());
 
 const startServer = async () => {
   try {
-    await mongoose.connect(config.MONGODB_URI, {
-      useNewUrlParser: true,
+    await mongoose.connect(MONGODB_URI, {
+      family: 4,
     });
     console.log('Подключено к MongoDB');
-    await app.listen(config.PORT);
-    console.log(`Сервер запущен на порте: ${config.PORT}`);
+    await app.listen(PORT);
+    console.log(`Сервер запущен на порте: ${PORT}`);
   } catch (err) {
     console.log('Ошибка подключения к MongoDB', err);
   }
 };
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
