8b08d3bc1927fbf2645c8943e7494d22151d690d
VadimRuslanch <Merzlyy.Pes@yandex.ru>
2023-06-30 17:27:43 +0300

    delite cors

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const { errors } = require('celebrate');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -26,7 +26,7 @@ const startServer = async () => {
   }
 };
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
