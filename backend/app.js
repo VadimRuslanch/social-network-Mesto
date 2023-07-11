da776d67d0af2a54c31d191e65c2c0baf612c3da
VadimRuslanch <Merzlyy.Pes@yandex.ru>
2023-07-12 00:27:21 +0300

    delete my  cors

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const corsAccess = require('./middlewares/corsAccess');
 const router = require('./routes');
 const config = require('./config');
 
@@ -17,22 +16,18 @@ const startServer = async () => {
     await mongoose.connect(config.MONGODB_URI, {
       family: 4,
     });
-    // eslint-disable-next-line no-console
     console.log('Подключено к MongoDB');
     await app.listen(config.PORT);
-    // eslint-disable-next-line no-console
     console.log(`Сервер запущен на порте: ${config.PORT}`);
   } catch (err) {
-    // eslint-disable-next-line no-console
     console.log('Ошибка подключения к MongoDB', err);
   }
 };
 
-// app.use(cors);
+app.use(cors);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
-app.use(corsAccess);
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
